3174a4ad8caf2e5d55ff6d2887b14377663fdc32
sunnivaasplund <sunnivaasplund@users.noreply.github.com>
2022-10-04 19:36:37 +0200

    Add project files.

---
+++ programbanken/programbanken/Program.cs
@@ -0,0 +1,31 @@
+﻿using Elastic.Clients.Elasticsearch;
+using Elastic.Transport;
+using Newtonsoft.Json;
+using programbanken;
+using RestSharp;
+
+var settings = new ElasticsearchClientSettings(new Uri("https://elastic-thesearch.es.devcoretools01.k8s.sr.se"))
+    .CertificateFingerprint("52:F3:A0:5E:71:20:54:23:2E:D2:D8:40:63:5E:93:E6:A2:07:F7:07:46:3B:DD:FD:0A:DF:DE:68:44:43:60:03")
+    .Authentication(new BasicAuthentication("thesearch-user", "895vNaDIsSkuGJhp"));
+
+var eclient = new ElasticsearchClient(settings);
+
+var client = new RestClient("http://programbanken-api.stotmhweb01.sr.se/");
+var request = new RestRequest("Post/GetModifiedPosts?changedAfter=2022-10-03&maxRowCount=10");
+
+var cancellationToken = CancellationToken.None;
+RestResponse response = await client.GetAsync(request, cancellationToken);
+
+if (response.IsSuccessful)
+{ 
+    var episodes = JsonConvert.DeserializeObject<List<ModifiedPostInfo>>(response.Content);
+
+    foreach (var episode in episodes)
+    {
+        var episdodeRequest = new RestRequest($"Post/GetPost?postHeaderId={episode.PostHeaderId}");
+        var episodeResponse = await client.GetAsync<SimplerPost>(episdodeRequest, cancellationToken);
+        var eresponse = await eclient.IndexAsync(episodeResponse, request => request.Index("programarchive-index"));
+    }
+
+    Console.WriteLine(response.Content);
+}
---
+++ programbanken/programbanken/SimplerPost.cs
@@ -0,0 +1,110 @@
+﻿namespace programbanken
+{
+    public class SimplerPost
+    {
+        public int Id { get; set; }
+
+        public string TablaUniqueId { get; set; }
+
+        public int Status { get; set; }
+
+        public DateTime LastChangeTime { get; set; }
+
+        public DateTime? LastPublished { get; set; }
+
+        public TimeSpan? Duration { get; set; }
+
+        public string Permission { get; set; }
+
+        public string PostType { get; set; }
+
+        public string ProgramSeriesTablaId { get; set; }
+
+        public string ProgramSeriesTablaName { get; set; }
+
+        public string Title { get; set; }
+
+        public string AlterativeTitle { get; set; }
+
+        public string MusicList { get; set; }
+
+        public string Description { get; set; }
+
+        public string Text { get; set; }
+
+        public string Transcript { get; set; }
+
+        public List<SimplerAudio> Audios { get; set; }
+
+        public List<SimplerPublication> Publications { get; set; }
+
+        public List<SimplerChildPost> ChildPosts { get; set; }
+    }
+
+    public class SimplerChildPost
+    {
+        public int Id { get; set; }
+
+        public string TablaUniqueId { get; set; }
+
+        public int Status { get; set; }
+
+        public DateTime LastChangeTime { get; set; }
+
+        public DateTime? LastPublished { get; set; }
+
+        public TimeSpan? Duration { get; set; }
+
+        public TimeSpan? StartOffset { get; set; }
+
+        public TimeSpan? EndOffset { get; set; }
+
+        public string Permission { get; set; }
+
+        public string PostType { get; set; }
+
+        public string Title { get; set; }
+
+        public string Paa { get; set; }
+
+        public string Ava { get; set; }
+
+        public string Description { get; set; }
+
+        public string Transcript { get; set; }
+
+        public string Text { get; set; }
+
+    }
+
+    public class SimplerPublication
+    {
+        public int? TablaProgramServiceId { get; set; }
+
+        public string TablaProgramServiceName { get; set; }
+
+        public string PublicationType { get; set; }
+
+        public DateTime? StartTime { get; set; }
+    }
+
+    public class SimplerAudio
+    {
+        public int AudioId { get; set; }
+
+        public string AudioUrl { get; set; }
+
+        public string AudioDataType { get; set; }
+
+        public string AudioType { get; set; }
+
+        public string AudioQuality { get; set; }
+    }
+
+    public class ModifiedPostInfo 
+    { 
+        public int PostHeaderId { get; set; } 
+        public DateTime LastChangeTime { get; set; } 
+        public bool IsDeleted { get; set; } 
+    }
+}
